ec2a32826c56e7dabdb2af37fffd21d28df7b914
Alex Echeverria <aleex.naahuel@outlook.com>
2026-01-29 21:51:35 -0300

    creo paginas principales

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import Hero from "../../components/Hero/Hero";
+import { HomePageContainerStyled } from "./HomePageStyles";
+
+const HomePage = () => {
+  return (
+    <HomePageContainerStyled>
+      <Hero />
+    </HomePageContainerStyled>
+  );
+}
+export default HomePage;
---
+++ src/pages/HomePage/HomePageStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomePageContainerStyled = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 0;
+`;
